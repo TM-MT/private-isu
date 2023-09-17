759f330b2d391c88aa5e048fa08bbd20187a95bd
TM-MT <tm-east@outlook.com>
2023-09-17 14:32:03 +0900

    Index を追加

--- webapp/sql/z-custom.sql
+++ webapp/sql/z-custom.sql
@@ -1,3 +1,4 @@
 -- ADD YOUR CUSTOM QUERY HERE
 USE isuconp;
 CREATE INDEX idx_post_id_created_at USING BTREE ON comments(post_id, created_at);
+CREATE INDEX idx_created_at USING BTREE ON posts(created_at);
